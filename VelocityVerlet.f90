c125022f88fcec20e421e8b5ab0421de57775f44
oadesida-warwick <o.adesida@warwick.ac.uk>
2020-12-20 18:52:47 +0000

    Fleshed out the Velocity Verlet Algorithm. Not checked for bugs yet

--- VelocityVerlet.f90
+++ VelocityVerlet.f90
@@ -4,23 +4,48 @@ module velocity_verlet
 
         contains
 
-                subroutine verlet_solver(field,init_pos,init_vel,init_acc,pos_hist,vel_hist,acc_hist)
+
+
+                !>function for working out the indices for the cell's current position
+                
+                function  current_cell(exact_pos) 
+                        real(kind=REAL64), dimension(2)                       :: exact_pos
+                        real(kind=int32) , dimension(2)                       :: current_cell
+
+                        current_cell(1) = floor((exact_pos(1) +1.0_dp)/dx) + 1 !>I know the pdf says "-1.0_dp"
+                        current_cell(2) = floor((exact_pos(1) +1.0_dp)/dy) + 1 !>But the maths seems weird to me here. Correct
+                                                                               !>me if I'm wrong
+
+                        return
+                end function
+
+                !>velocity verlet subroutine
+
+                subroutine verlet_solver(field,init_pos,init_vel,init_acc,pos_hist,vel_hist,acc_hist,dx,dy,dt)
                         real(kind=REAL64),  dimension(:,:),     intent(in)    :: field ! Electric field which needs to be taken in
                         real(kind=REAL64),  dimension(2),       intent(in)    :: init_pos, init_vel, init_acc !initial conditions
                         real(kind=REAL64),  dimension(0:1000,2),intent(out)   :: pos_hist, vel_hist, acc_hist !time histories
-                        integer(kind=int32),dimension(2)                      :: current_cell
+                        real(kind=REAL64),                      intent(in)    :: dt,dx,dy !pass it as a type? feels clunky
+                        integer(kind=int32)                                   :: i
+
 
 
                         pos_hist(0) = init_pos   !Adding initial conditions
                         vel_hist(0) = init_vel
+                       
+                        init_acc = -1* field(current_cell(init_pos))
 
-                        current_cell(1)  = floor((init_pos(1) + 1.0_dp)/dx) + 1 !I know the pdf says "-1.0_dp"
-                        current_cell(2)  = floor((init_pos(2) + 1.0_dp)/dy) + 1 !But the maths seems weird to me here. Correct me if
-                                                                                !I'm wrong
+                        vel_hist(0) = init_acc
 
-                        init_acc = field(current_cell(1),current_cell(2))       ! Can I just replace this with field(current_cell) ?
-                        vel_hist(0) =i init_acc
+                        do i = 1,1000
+                            pos_hist(i,:) = pos_hist(i-1,:) + vel_hist(i-1,:)*dt + 0.5*(vel_hist(i-1,:)**2)*dt**2
+                            acc_hist(i,:) = -1*field(current_cell(pos_hist(i-1,:)))
+                            vel_hist(i,:) = vel_hist(i-1,:) + 0.5*dt*(acc_hist(i,:)+acc_hist(i-1,:))
+                        end do 
 
+
+
+                end subroutine
 
 
 
