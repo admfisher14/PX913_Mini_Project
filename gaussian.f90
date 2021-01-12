595ece684a660f546b3044d9cc98cc4c9d15b74a
tmacmoto <75627139+tmacmoto@users.noreply.github.com>
2021-01-12 01:20:48 +0100

    Update gaussian.f90

--- gaussian.f90
+++ gaussian.f90
@@ -1,3 +1,4 @@
+!Simple module that gives the creates the Gaussian function
 MODULE GAUSSIAN
 
   USE ISO_FORTRAN_ENV
@@ -7,8 +8,10 @@ MODULE GAUSSIAN
   CONTAINS
  
   FUNCTION g(x,y,h,k,rho, sigma)
+    !rho and sigma determine the position of the Gaussian, while h and k are dx, dy.
     REAL(REAL64), INTENT(IN) :: rho, sigma, h, k
     REAL(REAL64) :: g
+    !This is for the loop variable
     INTEGER :: x,y
    
     g = EXP(-((h*(x-1)-1+sigma)/rho)**2-((k*(y-1)-1 +sigma)/ rho)**2)
