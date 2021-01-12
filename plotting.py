1c2c5467862669f0a8c72a9ddcae635541a0bf88
Adam Fisher <admfisher@live.com>
2021-01-12 15:14:04 +0000

    Added python plotting

---
+++ plotting.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np 
+import matplotlib 
+import matplotlib.pyplot as plt
+from netCDF4 import Dataset
+
+rootgrp = Dataset("results.nc", "a", format="NETCDF4")
+print (rootgrp)
+
+
+# In[11]:
+
+
+pos = np.array(rootgrp.variables['pos'][:])
+ex = np.array(rootgrp.variables['ex'][:])
+
+#ex = ex/(10**36)
+#print(np.std(ex))
+
+
+# In[23]:
+
+
+plt.axes()
+plt.axis("square")
+plt.axis([-1,1,-1,1])
+plt.scatter(pos[0],pos[1],1)
+plt.xlabel("x")
+plt.ylabel("y")
+plt.title("Position History of the Particle")
+plt.show()
+
+
+# In[32]:
+
+
+x = np.linspace(-1,1,np.shape(ex)[0]+1)
+y = np.linspace(-1,1,np.shape(ex)[0]+1)
+
+ 
+
+plt.axes()
+plt.axis("square")
+plt.axis([-1,1,-1,1])
+plt.pcolor(x,y,ex)
+plt.colorbar()
+plt.xlabel("x")
+plt.ylabel("y")
+plt.title(r"$\frac{\partial \phi}{\partial x}$, $E_x$",size =20)
+plt.show()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
